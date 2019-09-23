fdd2ae28ce9d07cf109817e2f6dc68394e9f492a
Xelanik <xelanik@gmail.com>
2019-09-23 13:14:19 +0300

    Updates

---
+++ precache-manifest.2e5381f9115b9286f70cce88dbc14ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2603badad063d086b43ea654e407ca9",
+    "url": "/Xelanik/teamvoy-test/index.html"
+  },
+  {
+    "revision": "10b1e0e123e3c9329bba",
+    "url": "/Xelanik/teamvoy-test/static/css/main.cd79211e.chunk.css"
+  },
+  {
+    "revision": "b17d83b694ce2335b5ad",
+    "url": "/Xelanik/teamvoy-test/static/js/2.b2f540c7.chunk.js"
+  },
+  {
+    "revision": "10b1e0e123e3c9329bba",
+    "url": "/Xelanik/teamvoy-test/static/js/main.ee2524ca.chunk.js"
+  },
+  {
+    "revision": "69da2f4900cca4f43c71",
+    "url": "/Xelanik/teamvoy-test/static/js/runtime~main.537a418d.js"
+  },
+  {
+    "revision": "605ebff03a482f2afbeedfad48b8cb4b",
+    "url": "/Xelanik/teamvoy-test/static/media/Pokemon_logo.605ebff0.svg"
+  }
+]);
